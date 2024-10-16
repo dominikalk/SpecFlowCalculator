41c2ffd0665956ec8dbd4d15f7850c7e1d83c978
Dominik Alkhovik <dominik.alk@hotmail.com>
2024-10-16 15:21:14 +0100

    initialise specflow

--- SpecFlowCalculator/Calculator.cs
+++ SpecFlowCalculator/Calculator.cs
@@ -2,5 +2,11 @@
 
 public class Calculator
 {
+    public int FirstNumber { get; set; }
+    public int SecondNumber { get; set; }
 
+    public int Add()
+    {
+        throw new NotImplementedException();
+    }
 }
